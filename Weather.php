8e0a542f8f9725dd88d7f1854131b15ae7ab055f
TobyMaxham <tobias.maxham@tricept.de>
2015-07-11 08:37:54 +0200

    Added the OpenWeather API URL Reaquester.

--- Weather.php
+++ Weather.php
@@ -8,8 +8,67 @@ namespace Tamarillo\Weather;
  */
 class Weather
 {
+
+	private static $lat, $lon, $id, $name, $url, $data;
+	private static $apiUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 	private $format;
 	private $defaultformat = '%oneLine%';
+
+	public static function getWeather($search)
+	{
+		if (is_array($search)) list(self::$lat, self::$lon) = $search;
+		elseif (is_int($search)) self::$id = $search;
+		else self::$name = $search;
+
+		return self::handleRequest();
+	}
+
+	/**
+	 * @return \Tamarillo\Weather\Weather
+	 */
+	private static function handleRequest()
+	{
+		self::buildUrl();
+
+		if (!Cache::has('weather')) {
+			echo 'request';
+			self::$data = file_get_contents(self::$url);
+			Cache::add('weather', self::$data, 1440);
+		} else self::$data = Cache::get('weather');
+
+		$last = date('mdy', json_decode(self::$data)->list[0]->dt);
+		if (date('mdy') != $last) self::$data = file_get_contents(self::$url);
+
+		$weather = new Weather(self::$data);
+
+		self::$name = self::$id = self::$lat = self::$lon =
+		self::$data = self::$url = NULL;
+
+		return $weather;
+	}
+
+	private static function buildUrl()
+	{
+		self::$url = self::$apiUrl;
+		if (self::$lat) $param = array('lat' => self::$lat, 'lon' => self::$lon);
+		elseif (self::$name) $param = array('q' => self::$name);
+		else {
+			self::$url .= '/city';
+			$param = array('id' => self::$id);
+		}
+
+		$param = array_add($param, 'APPID', self::apiKey());
+		$param = array_add($param, 'lang', 'de');
+		$param = array_add($param, 'units', 'metric');
+		$param = array_add($param, 'cnt', 7);
+
+		self::$url .= '?' . http_build_query($param);
+	}
+
+	private static function apiKey()
+	{
+		return \Config::get('admin.weather.key');
+	}
 
 	public function __toString()
 	{
@@ -37,9 +96,9 @@ class Weather
 
 	}
 
-	private function apiKey()
+	private function onlyMax()
 	{
-
+		return \Config::get('admin.weather.temp') == 'max';
 	}
 
 
--- readme.md
+++ readme.md
@@ -2,5 +2,6 @@
 
 [![Latest Stable Version](https://poser.pugx.org/tamarillo/weather/v/stable.svg)](https://packagist.org/packages/tamarillo/weather) [![Total Downloads](https://poser.pugx.org/tamarillo/weather/downloads.svg)](https://packagist.org/packages/tamarillo/weather) [![Latest Unstable Version](https://poser.pugx.org/tamarillo/weather/v/unstable.svg)](https://packagist.org/packages/tamarillo/weather) [![License](https://poser.pugx.org/tamarillo/weather/license.svg)](https://packagist.org/packages/tamarillo/weather)
 
-
 Helper classes for [OpenWeatherMap](http://openweathermap.org/API).
+
+This package was once part of the DengoCMS Framework. Now it's available on the Tamarillo project.
